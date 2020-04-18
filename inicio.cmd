7db570cc873991202c81ff5fc54f281fb34384b0
Isaac Alejandro Pimentel Morales <isaacapimentelm@gmail.com>
2020-04-17 20:43:03 -0500

    actualizado del cmd

--- inicio.cmd
+++ inicio.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-FOR %%X IN ("C:\MisProgramas\*.*") DO rundll32 shell32.dll,ShellExec_RunDLL %%X
+FOR %%X IN ("C:\Users\Public\MisProgramas\*.*") DO rundll32 shell32.dll,ShellExec_RunDLL %%X
